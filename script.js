34b6cabdaafd6a3892516ca0c28fef9405f44d44
Nelson <nsandrad20@gmail.com>
2025-04-01 21:09:34 -0700

    Implemented querySelectorAll which retrieves all instances of an element in a NodeList whereas querySelector simply returns the first instance of the element passed to the method

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let titleElement = document.getElementById("title");
 
 console.log(titleElement);
+
+const div = document.createElement("div");
+const p = document.createElement("p");
+document.body.append(div);
+div.innerText = "Hello world";
+p.innerText = "Goodbye";
+div.append(p);
+
+let descriptions = document.querySelectorAll("p");
+console.log(descriptions);
